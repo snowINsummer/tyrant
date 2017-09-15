5488e783f8c82df9d54abe2a67255a332f63b453
zhangli <45641783@>
2017-09-15 16:56:26 +0800

    测试接口

--- src/main/java/tyrant/controller/WSController.java
+++ src/main/java/tyrant/controller/WSController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import qa.exception.HTTPException;
+import qa.httpClient.HttpClientUtil;
 import qa.httpClient.ResponseInfo;
 import qa.utils.JSONFormat;
 import tyrant.common.constants.Constants;
@@ -15,6 +17,8 @@ import tyrant.service.WSService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.Map;
 //import javax.servlet.http.HttpSession;
 
@@ -76,6 +80,69 @@ public class WSController {
             rspData.setData(e.getMessage());
             e.printStackTrace();
         }
+        return rspData;
+    }
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public RspData test(HttpServletRequest request){
+        RspData rspData = new RspData();
+        HttpClientUtil httpClientUtil = new HttpClientUtil();
+        String url = "http://test-fk.xxd.com/verify/pre_addBaseInfo.htm";
+        Map header = new HashMap();
+        header.put("Content-Type","application/x-www-form-urlencoded");
+        header.put("cookie","gr_user_id=8641d984-5347-4b0a-ba27-a24f5e64b5b1; _ga=GA1.2.7A05B3DB-CAD0-440E-AD2D-900081333731.1504591862922; JSESSIONID=CF7BD9000BA2A970373B2498C2B283B9");
+        String json = "{\n" +
+                "    \"customerForm.accountBalance\": \"1\",\n" +
+                "    \"customerForm.age\": \"37\",\n" +
+                "    \"customerForm.belongArea\": \"0\",\n" +
+                "    \"customerForm.birthday\": \"1980-03-21\",\n" +
+                "    \"customerForm.carNum\": \"1\",\n" +
+                "    \"customerForm.carPrice\": \"1\",\n" +
+                "    \"customerForm.children\": \"\",\n" +
+                "    \"customerForm.constellation\": \"白羊座\",\n" +
+                "    \"customerForm.createdBy\": \"hejunhao\",\n" +
+                "    \"customerForm.createdTime\": \"2017-09-14 18:39:06.0\",\n" +
+                "    \"customerForm.customerVocation\": \"549\",\n" +
+                "    \"customerForm.domicile\": \"1\",\n" +
+                "    \"customerForm.education\": \"6\",\n" +
+                "    \"customerForm.email\": \"zhangli@xinxindai.com\",\n" +
+                "    \"customerForm.guarantor\": \"\",\n" +
+                "    \"customerForm.homePhone\": \"1\",\n" +
+                "    \"customerForm.houseDateEnd\": \"\",\n" +
+                "    \"customerForm.houseDateStart\": \"\",\n" +
+                "    \"customerForm.houseNum\": \"1\",\n" +
+                "    \"customerForm.housePrice\": \"1\",\n" +
+                "    \"customerForm.housingConditions\": \"0\",\n" +
+                "    \"customerForm.id\": \"28623\",\n" +
+                "    \"customerForm.idCardAddr\": \"1\",\n" +
+                "    \"customerForm.idCardAuth\": \"\",\n" +
+                "    \"customerForm.idNumber\": \"632626198003213899\",\n" +
+                "    \"customerForm.isOfficeHouse\": \"0\",\n" +
+                "    \"customerForm.maritalNumYear\": \"1\",\n" +
+                "    \"customerForm.maritalStatus\": \"1\",\n" +
+                "    \"customerForm.mobilePhonesNum\": \"13319878763\",\n" +
+                "    \"customerForm.name\": \"樊亚测试\",\n" +
+                "    \"customerForm.nowResidence\": \"1\",\n" +
+                "    \"customerForm.officeCondition\": \"0\",\n" +
+                "    \"customerForm.officeDateEnd\": \"\",\n" +
+                "    \"customerForm.officeDateStart\": \"\",\n" +
+                "    \"customerForm.officeTotalArea\": \"1\",\n" +
+                "    \"customerForm.sex\": \"0\",\n" +
+                "    \"customerForm.unitAddress\": \"1\",\n" +
+                "    \"customerForm.unitPhone\": \"1\",\n" +
+                "    \"customerForm.zodiac\": \"1\",\n" +
+                "    \"orderNo\": \"S3100170914006\"\n" +
+                "}";
+        Map data = JSONFormat.getMapFromJson(json);
+//        ResponseInfo responseInfo = httpClientUtil.executePostFormUrlencoded(url,false,header,data,null);
+        ResponseInfo responseInfo;
+        try {
+            responseInfo = httpClientUtil.executePatchWithHeaders(url,header,json);
+            rspData.setData(responseInfo);
+        } catch (HTTPException e) {
+            rspData.setData(e);
+            e.printStackTrace();
+        }
+//        rspData.setData(responseInfo);
         return rspData;
     }
 
