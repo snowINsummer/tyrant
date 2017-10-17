6d00769ade86bfad192476718a61c412c43f8ea2
snowINsummer <45641783@qq.com>
2017-10-17 15:50:34 +0800

    删除import

--- src/main/java/tyrant/service/impl/ProductServiceImpl.java
+++ src/main/java/tyrant/service/impl/ProductServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import qa.utils.DateFormat;
 import tyrant.common.constants.Constants;
-import tyrant.common.enums.TestEnum;
 import tyrant.dao.IProductDao;
 import tyrant.entity.Product;
 import tyrant.service.ProductService;
